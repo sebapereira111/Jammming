2452a4a96fe8d07412fea7169cb983e12e09faee
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2025-07-29 14:49:27 -0300

    Correccion de errores en playlist y cambio de funcionamiento de boton X

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -5,24 +5,15 @@ import Track from '../Track/Track'
 function Playlist({ listaDeMusicas, setListaDeMusicas }) {
     /* Variable para el nombre de la playlist */
     const [nombrePlaylist, setNombrePlaylist] = useState("");
-    /* Variable para activar y dasactivar el boton ❌ */
-    const [botonXNombrePlaylistActivo, setBotonXNombrePlaylistActivo] = useState(false);
 
-    /* Para modificar el nombre de la playlist y unificar con el texto.
-    Tambien hace aparecer y desaparecer el boton ❌ */
+    /* Para modificar el nombre de la playlist y unificar con el texto */
     function handleChangeNombrePlaylist(e) {
         setNombrePlaylist(e.target.value)
-        if (e.target.value == "") {
-            setBotonXNombrePlaylistActivo(false);
-        } else {
-            setBotonXNombrePlaylistActivo(true);
-        }
     }
 
     /* Para borrar el nombre de la playlist con la X */
     function handleBorrarNombrePlaylist() {
         setNombrePlaylist("");
-        setBotonXNombrePlaylistActivo(false);
     }
 
     /* Borra la playlist completa */
@@ -50,7 +41,7 @@ function Playlist({ listaDeMusicas, setListaDeMusicas }) {
                             placeholder='Nombre de playlist'
                             onChange={handleChangeNombrePlaylist}
                         />
-                        <button id='botonXNombrePlaylist' type='button' className={botonXNombrePlaylistActivo ? "playlist-boton-x-nombre-activo" : "playlist-boton-x-nombre-inactivo"} onClick={handleBorrarNombrePlaylist}>❌</button>
+                        <button type='button' className='playlist-boton-x-nombre' disabled={!nombrePlaylist} onClick={handleBorrarNombrePlaylist}>❌</button>
                     </div>
                 </form>
                 {listaDeMusicas.map((item, index) => (<Track index={index} musica={item} boton={"-"} setListaDeMusicas={setListaDeMusicas}/>))}
