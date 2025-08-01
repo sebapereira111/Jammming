7633bfd5f9ee339ccc519715133a2cfa94773a88
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2025-08-01 14:50:31 -0300

    En track se agrega dialog box con la imagen del disco mas grande al hacer hover por la imagen

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import './Track.css'
 
 function Track({ index, musica, boton, setListaDeMusicas }) {
+    const [mostrarCoverArt, useMostrarCoverArt] = useState(false);
+
     function handleAgregarOEliminar() {
         if (boton == "+") {
             setListaDeMusicas(prev => [...prev, {
@@ -15,11 +18,27 @@ function Track({ index, musica, boton, setListaDeMusicas }) {
         }
     }
 
+    function handleMostrarCoverArt() {
+        useMostrarCoverArt(true);
+    }
+
+    function handleOcultarCoverArt() {
+        useMostrarCoverArt(false);
+    }
+
     return (
         <>
+            <dialog className='track-dialog' open={mostrarCoverArt}>
+                <img className='track-cover-art-grande' src={musica.coverArt} alt="" />
+                <div className='track-dialog-text'>
+                    <p className='track-texto-nombre'>{musica.name}</p>
+                    <p className='track-texto-artista'>{musica.artist}</p>
+                    <p className='track-texto-album'>{musica.album}</p>
+                </div>
+            </dialog>
             <div className='track-seccion'>
                 <div className='track-contenido'>
-                    <img className='track-cover-art' src={musica.coverArt} alt="" />
+                    <img onMouseEnter={handleMostrarCoverArt} onMouseLeave={handleOcultarCoverArt} className='track-cover-art' src={musica.coverArt} alt="" />
                     <div className='track-contenido-texto'>
                         <p className='track-texto-nombre'>{musica.name}</p>
                         <p className='track-texto-artista'>{musica.artist}</p>
